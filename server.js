551c69373b099ca2c820034b38a1b97d167e3154
Matteo Collina <hello@matteocollina.com>
2014-12-23 13:03:45 +0100

    Initial import.

---
+++ server.js
@@ -0,0 +1,34 @@
+#! /usr/bin/env node
+
+var http    = require('http');
+var redis   = require('redis');
+var concat  = require('concat-stream');
+var client  = redis.createClient(6379, process.env.DOCKER_HOST_IP || 'localhost');
+var server  = http.createServer();
+
+server.on('request', function(req, res) {
+  if (req.method === 'GET') {
+    client.get(req.path, function(err, value) {
+      if (err) {
+        res.statusCode = 500;
+        res.end(err.message);
+        return;
+      }
+      res.end(value);
+    });
+  } else {
+    req.pipe(concat(function(list) {
+      client.set(req.path, list.toString(), function(err) {
+        if (err) {
+          res.statusCode = 500;
+          res.end(err.message);
+          return;
+        }
+        res.statusCode = 204;
+        res.end();
+      })
+    }));
+  }
+});
+
+server.listen(3000);
